5b476d6167bcf46ea3bf20725fa5378e9eb6d4ae
Lucas <lulu.mig@orange.fr>
2021-10-06 10:10:45 +0200

    First commit

--- src/allmenu.c
+++ src/allmenu.c
@@ -9,48 +9,9 @@ void allMenu(int choix){
     switch (choix)
     {
         case 1:
-            char nbr_bin[ARRAY_SIZE];          
-            printf("Entrez un nombre binaire : ");
-            scanf("%s",&nbr_bin);
-            dec = bintodec(nbr_bin);
-            printf("Votre nombre en décimal est : %d\n", dec);
+            
             break;
-        case 2:
-            char nbr_hexa[ARRAY_SIZE];           
-            printf("Entrez un nombre hexadécimal : ");
-            scanf("%s",&nbr_hexa);
-            dec = hexatodec(nbr_hexa);
-            printf("Votre nombre en décimal est : %d\n", dec);
-            break;
-        case 3 :
-            num;
-            printf("Saisir un nombre decimal: \n");
-            scanf("%d",&num);
-            result = dectohexa(num);
-            int sizetab = sizeof(result)-1;
-            for (int i = sizetab; i >= 0; i--)
-            {
-                printf("%c",result[i]);
-            }
-            printf("\n");
-            break;
-        case 4:
-            num;
-            printf("Saisir un nombre decimal: \n");
-            scanf("%d",&num);
-            result = dectobin_v1(num);
-            printf("%s",result);
-            break;
-        case 5:
-            num;
-            printf("Saisir un nombre decimal: \n");
-            scanf("%d",&num);
-            result = dectobin_v2(num);
-            for (int i = sizetab; i >= 0; i--)
-            {
-                printf("%c",result[i]);
-            }
-            break;
+        
         default:
             break;
     }
